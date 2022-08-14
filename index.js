01c31361bc4b5b6ec04e170a6eeac135302de6af
Preston West <prestonmontewest@gmail.com>
2022-08-13 20:01:22 -0500

    Made the unique-digit years calc importable

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 #!/usr/bin/env node
 
+function getUniqueDigitYears(startYear, endYear) {
+    const years = [];
+
+    for (let year = startYear; year <= endYear; year++) {
+        let yearStr = `${year}`;
+        let duplicateFound = false;
+
+        while (yearStr.length - 1) {
+            const digit = yearStr.charAt(0);
+            yearStr = yearStr.slice(1);
+
+            if (yearStr.includes(digit)) {
+                duplicateFound = true;
+                break;
+            }
+        }
+
+        if (!duplicateFound) {
+            years.push(year);
+        }
+    }
+
+    return years;
+}
+
+module.exports = getUniqueDigitYears;
+
+
+
+// if not script, exit
+if (require.main !== module) {
+    return;
+}
+
 try {
     const [startYear, endYear] = processArgs();
     console.log(getUniqueDigitYears(startYear, endYear).join(', '));
@@ -8,7 +42,6 @@ try {
     process.exitCode = 1;
 }
 
-// helpers
 function processArgs() {
     const args = process.argv.slice(2);
 
@@ -37,29 +70,4 @@ function processArgs() {
     }
 
     return [startYear, endYear];
-}
-
-function getUniqueDigitYears(startYear, endYear) {
-    const years = [];
-
-    for (let year = startYear; year <= endYear; year++) {
-        let yearStr = `${year}`;
-        let duplicateFound = false;
-
-        while (yearStr.length - 1) {
-            const digit = yearStr.charAt(0);
-            yearStr = yearStr.slice(1);
-
-            if (yearStr.includes(digit)) {
-                duplicateFound = true;
-                break;
-            }
-        }
-
-        if (!duplicateFound) {
-            years.push(year);
-        }
-    }
-
-    return years;
 }
