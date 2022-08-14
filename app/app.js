adc77fd5a49b60df71e7cf1c0dbb6de688fa1254
Preston West <prestonmontewest@gmail.com>
2022-08-13 21:50:57 -0500

    Restructured package to separate app and lib

--- app/app.js
+++ app/app.js
@@ -1,38 +1,6 @@
 #!/usr/bin/env node
 
-function getUniqueDigitYears(startYear, endYear) {
-    const years = [];
-
-    for (let year = startYear; year <= endYear; year++) {
-        let yearStr = `${year}`;
-        let duplicateFound = false;
-
-        while (yearStr.length - 1) {
-            const digit = yearStr.charAt(0);
-            yearStr = yearStr.slice(1);
-
-            if (yearStr.includes(digit)) {
-                duplicateFound = true;
-                break;
-            }
-        }
-
-        if (!duplicateFound) {
-            years.push(year);
-        }
-    }
-
-    return years;
-}
-
-module.exports = getUniqueDigitYears;
-
-
-
-// if not script, exit
-if (require.main !== module) {
-    return;
-}
+const getUniqueDigitYears = require("../lib/unique-digit-years");
 
 try {
     const [startYear, endYear] = processArgs();
