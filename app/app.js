aac0ee2350b94a06bf96eb8bd673606c1bc67e99
Preston West <prestonmontewest@gmail.com>
2023-08-23 22:22:54 -0500

    Updated unique-digit lib function to be generator

--- app/app.js
+++ app/app.js
@@ -2,40 +2,44 @@
 
 const getUniqueDigitYears = require("../lib/unique-digit-years");
 
-try {
-    const [startYear, endYear] = processArgs();
-    console.log(getUniqueDigitYears(startYear, endYear).join(', '));
-} catch (err) {
-    console.error(`Error: ${err.message}`);
-    process.exitCode = 1;
+function processArgs() {
+  const args = process.argv.slice(2);
+
+  if (args.length < 2) {
+    throw new Error("Start and end years are required.");
+  }
+
+  if (args.length > 2) {
+    throw new Error("Only provide start and end years.");
+  }
+
+  const startYear = +args[0];
+  const endYear = +args[1];
+
+  if (
+    !Number.isInteger(startYear) ||
+    startYear < 0 ||
+    !Number.isInteger(endYear) ||
+    endYear < 0
+  ) {
+    throw new Error("Start and end years should be nonnegative integers.");
+  }
+
+  if (startYear >= endYear) {
+    throw new Error("Start year should come before end year.");
+  }
+
+  return [startYear, endYear];
 }
 
-function processArgs() {
-    const args = process.argv.slice(2);
-
-    if (args.length < 2) {
-        throw new Error('Start and end years are required.');
-    }
-
-    if (args.length > 2) {
-        throw new Error('Only provide start and end years.');
-    }
-
-    const startYear = +args[0];
-    const endYear = +args[1];
-
-    if (
-        !Number.isInteger(startYear) ||
-        startYear < 0 ||
-        !Number.isInteger(endYear) ||
-        endYear < 0
-    ) {
-        throw new Error('Start and end years should be nonnegative integers.');
-    }
-
-    if (startYear >= endYear) {
-        throw new Error('Start year should come before end year.');
-    }
-
-    return [startYear, endYear];
+try {
+  const [startYear, endYear] = processArgs();
+  console.time("getUniqueDigitYears");
+  for (const year of getUniqueDigitYears(startYear, endYear)) {
+    console.log(year);
+  }
+  console.timeEnd("getUniqueDigitYears");
+} catch (err) {
+  console.error(`Error: ${err.message}`);
+  process.exitCode = 1;
 }
